d50aa19ad94b50f479bf1ab4d1baadf3d722d40b
Zachary <Zachary@10.30.19.218>
2016-03-23 19:17:24 -0500

    new branch for JPA version

--- src/main/java/edu/wctc/zcs/bookwebapp/model/Author.java
+++ src/main/java/edu/wctc/zcs/bookwebapp/model/Author.java
@@ -1,67 +1,61 @@
 package edu.wctc.zcs.bookwebapp.model;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author Zachary
  */
-public class Author {
-    private int authorID;
+@Entity
+@Table(name = "author")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Author.findAll", query = "SELECT a FROM Author a"),
+    @NamedQuery(name = "Author.findByAuthorId", query = "SELECT a FROM Author a WHERE a.authorId = :authorId"),
+    @NamedQuery(name = "Author.findByAuthorName", query = "SELECT a FROM Author a WHERE a.authorName = :authorName"),
+    @NamedQuery(name = "Author.findByDateAdded", query = "SELECT a FROM Author a WHERE a.dateAdded = :dateAdded")})
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "author_id")
+    private Integer authorId;
+    @Size(max = 45)
+    @Column(name = "author_name")
     private String authorName;
+    @Column(name = "date_added")
+    @Temporal(TemporalType.DATE)
     private Date dateAdded;
 
     public Author() {
-        this.dateAdded = new Date();
     }
 
-    public Author(int authorID) {
-        this.authorID = authorID;
-        this.dateAdded = new Date();
+    public Author(Integer authorId) {
+        this.authorId = authorId;
     }
 
-    public Author(int authorID, String authorName, Date dateAdded) {
-        this.authorID = authorID;
-        this.authorName = authorName;
-        this.dateAdded = dateAdded;
+    public Integer getAuthorId() {
+        return authorId;
     }
 
-    @Override
-    public String toString() {
-        return "Author{" + "authorID=" + authorID + ", authorName=" + authorName + '}';
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 29 * hash + this.authorID;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Author other = (Author) obj;
-        if (this.authorID != other.authorID) {
-            return false;
-        }
-        return true;
-    }
-
-    public int getAuthorID() {
-        return authorID;
-    }
-
-    public void setAuthorID(int authorID) {
-        this.authorID = authorID;
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
     }
 
     public String getAuthorName() {
@@ -79,6 +73,30 @@ public class Author {
     public void setDateAdded(Date dateAdded) {
         this.dateAdded = dateAdded;
     }
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (authorId != null ? authorId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Author)) {
+            return false;
+        }
+        Author other = (Author) object;
+        if ((this.authorId == null && other.authorId != null) || (this.authorId != null && !this.authorId.equals(other.authorId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edu.wctc.zcs.bookwebapp.model.Author[ authorId=" + authorId + " ]";
+    }
     
 }
